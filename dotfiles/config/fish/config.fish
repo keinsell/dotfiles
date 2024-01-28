1a8af18b079eb15f5a37d6c20366972d2a5996d7
Jakub Olan <keinsell@protonmail.com>
2024-01-28 22:47:21 +0100

    .

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+alias parus "paru -S --noconfirm --needed"
+aliast parur "paru -R --noconfirm -u"
+
 # Install Zoxide
 zoxide init fish | source
